c022905e202b0753729d8bdfaa55f7463e8009db
James Dempsey <x00189475@mytudublin.ie>
2022-11-30 17:53:14 +0000

    cart and shop apps

--- venusproject/venusproject/settings.py
+++ venusproject/venusproject/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'users',
     'customers',
     'home',
+    'shop',
+    'cart',
+
 ]
 
 MIDDLEWARE = [
@@ -124,6 +127,15 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder"
+]
+MEDIA_URL ='/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
